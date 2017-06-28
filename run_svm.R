9aac357b278c58b44eab834e7c571e02f8e013d8
Pulkit Jain <Pulkit Jain>
2017-06-28 11:26:06 -0500

    Relative address for reading data.

--- run_svm.R
+++ run_svm.R
@@ -2,24 +2,26 @@
 # Tuning via, cross validation takes some serious time!!!
 
 library(e1071)
-
+library(tidyverse)
 
 
 # input feature table in data
-svm_data <- read.csv("/general/group/pinfo/pulkit/rec_link/round2/feature_table.csv",
+svm_data <- read.csv("./round2/feature_table.csv",
                      stringsAsFactors=FALSE)
-svm_data <- svm_data[,2:22]
+# svm_data <- svm_data[,2:22]
 
 
 svm_data$response <- as.factor(svm_data$response)
 
-
+set.seed(1004)
 train <- sort(sample(nrow(svm_data), 0.8*nrow(svm_data)))
 nc_run.test <- svm_data[-train,]
 nc_run.train <- svm_data[train,]
 
 
-svmfit <- svm(response~., data=nc_run.train, kernal = "radial", scale = F
+
+
+svmfit <- svm(response~.-Indice, data=nc_run.train, kernal = "radial"
               ,cost = 10 , gamma=1)
 
 
@@ -29,6 +31,8 @@ summary(svmfit)
 yhat_test <- predict(svmfit, nc_run.test)
 table(test = yhat_test, true = nc_run.test$response)
 
+nc_run.test %>% filter(yhat_test != response) %>% View()
+nc_run.test[yhat_test != nc_run.test$response,]
 
 tc <- tune.control(cross = 5)
 
@@ -37,6 +41,8 @@ tune_radial_svm <- tune.svm(response~., data= svm_data, kernel="radial",
                             gamma = 0.5*(seq(1,5)) )
 # )
 # , tunecontrol = tc)
+
+bind_rows(pos_pairs,neg_pairs)[-train,] %>% filter(yhat_test != nc_run.test$response)
 
 
 tune_radial_svm
